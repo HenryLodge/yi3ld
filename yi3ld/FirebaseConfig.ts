80f925d9d678a911871d172e043afb1c2c1da00c
HenryLodge <hlcodes01@gmail.com>
2026-02-06 17:18:17 -0500

    firebase phone auth

---
+++ yi3ld/FirebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAhDxt6bLxuzPyRqkkuLI6JV9FBgTux0g",
+  authDomain: "yi3ld-8c8bd.firebaseapp.com",
+  projectId: "yi3ld-8c8bd",
+  storageBucket: "yi3ld-8c8bd.firebasestorage.app",
+  messagingSenderId: "531410606307",
+  appId: "1:531410606307:web:70c7fd7fb1c6fafc4acdd6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
